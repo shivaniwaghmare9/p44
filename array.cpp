f2527d6bb8cac02cd346db91b3f4a1b4a95556e5
shivani <shivaniw69@gmail.com>
2025-01-31 13:05:37 +0530

    array

--- array.cpp
+++ array.cpp
@@ -281,7 +281,76 @@ int main()
    }
    }
 }
-        
+
+
+     // Online C++ compiler to run C++ program online
+///=================================DUPLICTAE NUMBER=======================
+#include <iostream>
+using namespace std;
+int main() {
+  int arr[]={4,2,1,2,1};
+  for(int i=0; i<5; i++) {
+      bool duplicate=false;
+      if(arr[i]!=-1){
+       for (int j=i+1; j<5; j++){
+          if(arr[i]==arr[j]){
+              arr[j]=-1;
+              duplicate=true;
+              
+              
+          }
+      }
+      if(duplicate)
+      {
+      
+      cout<<arr[i]<<endl;
+      }
+  }
+  }
+}
+// Online C++ compiler to run C++ program online
+///=================================unique NUMBER=======================
+#include <iostream>
+using namespace std;
+int main() {
+  int arr[]={4,2,1,2,1};
+  for(int i=0; i<5; i++) {
+      bool duplicate=false;
+      if(arr[i]!=-1){
+       for (int j=i+1; j<5; j++){
+          if(arr[i]==arr[j]){
+              arr[j]=-1;
+              duplicate=true;
+              
+              
+          }
+      }
+      if(!duplicate&&arr[i]!=-1)
+      {
+      
+      cout<<arr[i]<<endl;
+      }
+  }
+  }
+}
+  
+  
+
+
+
+   
+   
+
+
+  
+  
+
+
+
+   
+   
+
+   
        
     
 
