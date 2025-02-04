79d28015b1639f13e3f6016e75555d5dce920d8a
shivani <shivaniw69@gmail.com>
2025-02-04 19:35:03 +0530

    factorial

--- array.cpp
+++ array.cpp
@@ -497,6 +497,75 @@ int main() {
    cout<<"majority element:"<<majority;
    return 0;
 }
+// Online C++ compiler to run C++ program online
+///non-return type  --withoutparameter=================
+#include <iostream>
+using namespace std;
+void show(){
+    cout<<"hello";
+}
+int main() {
+   show();
+   show();
+   show();
+   
+   
+ 
+}
+// Online C++ compiler to run C++ program online
+///non-return type  --withparameter=================
+#include <iostream>
+using namespace std;
+void show(string a){
+    cout<<a;
+}
+int main() {
+   show("shivani");
+  
+   
+   
+ 
+}
+// Online C++ compiler to run C++ program online
+///return type  --withoutparameter=================
+#include <iostream>
+using namespace std;
+int show(){
+    int a=40;
+    int b=30;
+    int c=a+b;
+    cout<<c;
+    return c;
+    
+}
+int main() {
+   show();
+  
+   
+   
+ 
+}
+// Online C++ compiler to run C++ program online
+///non-return type  --withparameter(factorial=================
+#include <iostream>
+using namespace std;
+void factorial(int num){
+    int ans=1;
+    for(int i=1; i<=num; i++)
+    {
+        ans=ans*i;
+    }
+    cout<<ans;
+}
+int main() {
+   int a;
+   cout<<"enter a number";
+   cin>>a;
+   factorial(a);
+   return 0;
+   
+  
+}   
     
   
 
