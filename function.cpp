6abae24c9b3ada1dc508b6946126136e3d4095e9
shivani <shivaniw69@gmail.com>
2025-02-11 13:58:13 +0530

    recursion

--- function.cpp
+++ function.cpp
@@ -110,39 +110,7 @@ int main() {
    
 }
 
-// Online C++ compiler to run C++ program online
-//=============================FUNCTION(recursion[factorial]I)================================
-#include <iostream>
-using namespace std;
-int sum(int n){
-    if(n<=1){
-        return n;
-    }
-    return n*sum(n-1);
-}
-int main(){
-   
-    
-                                    //output=6
-    cout<<sum(3);                          
-}
 
-// Online C++ compiler to run C++ program online
-//=============================FUNCTION(recursion[sum)]================================
-#include <iostream>
-using namespace std;
-int sum(int n){
-    if(n<=1){
-        return n;
-    }
-    return n+sum(n-1);
-}
-int main(){
-   // int n(5);
-    
-   
-    cout<<sum(5);          //output=15
-}
 
 // Online C++ compiler to run C++ program online
 //=============================FUNCTION([factorial]I)================================
---
+++ recursion.cpp
@@ -0,0 +1,50 @@
+// Online C++ compiler to run C++ program online
+//=============================FUNCTION(recursion[factorial]I)================================
+#include <iostream>
+using namespace std;
+int sum(int n){
+    if(n<=1){
+        return n;
+    }
+    return n*sum(n-1);
+}
+int main(){
+   
+    
+                                    //output=6
+    cout<<sum(3);                          
+}
+
+// Online C++ compiler to run C++ program online
+//=============================FUNCTION(recursion[sum)]================================
+#include <iostream>
+using namespace std;
+int sum(int n){
+    if(n<=1){
+        return n;
+    }
+    return n+sum(n-1);
+}
+int main(){
+   // int n(5);
+    
+   
+    cout<<sum(5);          //output=15
+}
+
+// Online C++ compiler to run C++ program online
+///=============================fibonaachi with recursion==============================
+#include <iostream>
+using namespace std;
+int sum(int n)
+{
+if(n==1 ||  n==0){
+    return n;
+}
+return sum(n-1)+sum(n-2);
+}
+int main() {                             //output=01123
+    for(int i=0; i<5; i++){
+    cout<<sum(i);
+    }
+}
