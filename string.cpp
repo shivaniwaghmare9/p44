4d8399325500efa8cf29d260d734d73fab47633f
shivani <shivaniw69@gmail.com>
2025-02-12 23:36:09 +0530

    new

--- string.cpp
+++ string.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 int main() {
  string str="KANAK";
- string ans=str;
+ string ans=str;    
  int i=0;
  int j=str.length()-1;
  while(i<=j){
@@ -44,4 +44,41 @@ int main() {
  
  cout<<str;
  return 0;
+}
+
+// Online C++ compiler to run C++ program online
+//=================================ANAGRAM==============================
+#include <iostream>
+using namespace std;
+int main() {
+    string str1="anagram";
+    string str2="aaangrm";
+    if(str1.length()!=str2.length())
+    {
+        cout<<"not a anagram";
+        return 0;
+    }
+    
+    
+    for(int i=0; i<str1.length(); i++)
+    {
+        bool found=false;
+        for(int j=0; j<str2.length();j++)
+        {
+           
+          if( str1[i]==str2[j])
+          {
+            str2[j]='#';
+            found=true;
+            break;
+          }
+            
+            }
+    
+    if(!found==false){
+        cout<<"anagram";
+        return 0;
+    }
+    }
+    cout<<"anagram";
 }
