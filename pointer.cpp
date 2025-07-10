1599f2179c2fe20e81f566c92f6119b0c2f1c200
shivani <shivaniw69@gmail.com>
2025-07-10 23:16:37 +0530

    commit

--- pointer.cpp
+++ pointer.cpp
@@ -1,35 +1,5 @@
 
 
-// // Online C++ compiler to run C++ program online
-// //============================POINTER=============================================
-// #include <iostream>
-// using namespace std;
-// struct student{
-//     private:
-//     int roll;
-//     string name;
-//     public:
-//     void display(int x, string y){
-//         roll=x;
-//         name=y;
-//     }
-//     void show(){
-        
-//         cout<<"name of student:"<<name<<endl;
-//         cout<<"roll of student:"<<roll<<endl;
-//     }
-// };
-// int main() {
-//    student *var1;
-//    student var2;
-//    var2.display(102,"shivani");
-//    var1=&var2;
-//    var1->show();
-   
-// }
-
-
-
 // Online C++ compiler to run C++ program online
 //============================POINTER=============================================
 #include <iostream>
@@ -57,4 +27,3 @@ int main() {
    var1->show();
    
 }
-
